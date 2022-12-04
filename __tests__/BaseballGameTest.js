5b1c9080238d5247a4c36097033cb19d64688601
ReturnReason <reasonz@daum.net>
2022-12-04 18:27:44 +0900

    test(BaseballGame): countBall 메서드 테스트

--- __tests__/BaseballGameTest.js
+++ __tests__/BaseballGameTest.js
@@ -1,12 +1,21 @@
 const BaseballGame = require('../src/model/BaseballGame');
 
 describe('⚾ BaseballGame 클래스 테스트', () => {
-  test('컴퓨터와 유저의 숫자 3개가 같은 자리, 같은 수이면 3스트라이크이다.', () => {
+  test('countStrike 메서드 테스트 : 컴퓨터와 유저의 숫자 3개가 같은 자리, 같은 수이면 3스트라이크이다.', () => {
     const baseballGame = new BaseballGame();
     const computerNumbers = [1, 2, 3];
     const userNumber = [1, 2, 3];
     const result = baseballGame.countStrike(computerNumbers, userNumber);
 
     expect(result).toBe(3);
+  });
+
+  test('countBall 메서드 테스트 : 컴퓨터와 유저의 숫자 2개가 같지만, 자리가 다르면 2볼이다.', () => {
+    const baseballGame = new BaseballGame();
+    const computerNumbers = [1, 2, 3];
+    const userNumber = [4, 1, 2];
+    const result = baseballGame.countBall(computerNumbers, userNumber);
+
+    expect(result).toBe(2);
   });
 });
