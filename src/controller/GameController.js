fe9f03266fea6b6e4ec99f61a83bda784abdadde
ReturnReason <reasonz@daum.net>
2022-12-04 18:50:01 +0900

    feat(GameController): 유저가 입력한 숫자와 컴퓨터의 숫자를 비교하여 스트라이크, 볼 카운트를 받아 아웃풋 뷰에게 출력하도록 요청하는 메서드 구현

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -2,26 +2,40 @@ const Validator = require('../validate/Validator');
 const InputView = require('../view/InputView');
 const OutputView = require('../view/OutputView');
 const Computer = require('../model/Computer');
+const BaseballGame = require('../model/BaseballGame');
 
 class GameController {
   #inputView;
   #outputView;
   #computer;
+  #baseballGeme;
 
   constructor() {
     this.#inputView = new InputView();
     this.#outputView = new OutputView();
+
     this.#computer = new Computer();
+    this.#baseballGeme = new BaseballGame();
+
     this.validator = new Validator();
   }
 
   start() {
     const userInput = (input) => {
       this.validator.validCheckBundle(input);
+      this.compare(input);
     };
 
     this.#outputView.printStart();
     this.#inputView.readNumber(userInput);
+  }
+
+  compare(userInput) {
+    const computerNumbers = this.#computer.getGameNumbers();
+    const strike = this.#baseballGeme.countStrike(computerNumbers, userInput);
+    const ball = this.#baseballGeme.countBall(computerNumbers, userInput);
+
+    this.#outputView.printStrikeBall(strike, ball);
   }
 }
 
