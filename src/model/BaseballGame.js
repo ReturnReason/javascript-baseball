00c875d666197a9106b4d1c6cde050f0f7485e8c
ReturnReason <reasonz@daum.net>
2022-12-04 18:28:38 +0900

    feat(BaseballGame): 컴퓨터 숫자와 유저 숫자가 위치는 다르지만 포함한 숫자가 동일하면 볼을 증가시키는 메서드 구현

--- src/model/BaseballGame.js
+++ src/model/BaseballGame.js
@@ -10,6 +10,18 @@ class BaseballGame {
 
     return strike;
   }
+
+  countBall(computerNumbers, userNumbers) {
+    let ball = 0;
+
+    computerNumbers.forEach((computerNumber, i) => {
+      if (computerNumber !== userNumbers[i] && computerNumbers.includes(userNumbers[i])) {
+        ball += 1;
+      }
+    });
+
+    return ball;
+  }
 }
 
 module.exports = BaseballGame;
