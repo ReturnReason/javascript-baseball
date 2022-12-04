151b99d64318dd6e30dd007e2043ea6af272f908
ReturnReason <reasonz@daum.net>
2022-12-04 19:24:31 +0900

    feat(OutputView): 볼, 스트라이크를 출력하는 메서드 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,10 +1,22 @@
 const { PRINT_MESSAGE } = require('../constant/messages');
+const { THREE_STRIKE } = require('../constant/baseball');
 const { print, close } = require('../utils/missionUtils');
 
 class OutputView {
   printStart() {
     const { start } = PRINT_MESSAGE;
     return print(start);
+  }
+
+  printStrikeBall(strikeCount, ballCount) {
+    const { ball, strike, nothing } = PRINT_MESSAGE;
+
+    if (strikeCount === THREE_STRIKE) return this.printEndGame();
+    if (!strikeCount && !ballCount) return print(`${nothing}`);
+    if (strikeCount && !ballCount) return print(`${strikeCount}${strike}`);
+    if (!strikeCount && ballCount) return print(`${ballCount}${ball}`);
+
+    return print(`${ballCount}${ball} ${strikeCount}${strike}`);
   }
 }
 
