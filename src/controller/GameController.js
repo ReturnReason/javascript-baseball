8a8406383b5622395c77572f00b4536dafec84bc
ReturnReason <reasonz@daum.net>
2022-12-04 19:38:26 +0900

    feat(GameController): 3스트라이크로 게임 종료시 사용자로부터 게임을 재시작할지, 종료할 지 묻는 로직 추가

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -3,39 +3,58 @@ const InputView = require('../view/InputView');
 const OutputView = require('../view/OutputView');
 const Computer = require('../model/Computer');
 const BaseballGame = require('../model/BaseballGame');
+const { THREE_STRIKE } = require('../constant/baseball');
+const { COMMAND } = require('../constant/baseball');
 
 class GameController {
   #inputView;
   #outputView;
   #computer;
-  #baseballGeme;
+  #baseballGame;
 
   constructor() {
     this.#inputView = new InputView();
     this.#outputView = new OutputView();
-
     this.#computer = new Computer();
-    this.#baseballGeme = new BaseballGame();
-
+    this.#baseballGame = new BaseballGame();
     this.validator = new Validator();
+
+    this.#outputView.printStart();
   }
 
   start() {
+    this.#computer.initGameNumbers();
+
     const userInput = (input) => {
       this.validator.validCheckBundle(input);
       this.compare(input.split('').map(Number));
+      this.#inputView.readNumber(userInput);
     };
 
-    this.#outputView.printStart();
     this.#inputView.readNumber(userInput);
   }
 
   compare(userInput) {
     const computerNumbers = this.#computer.getGameNumbers();
-    const strike = this.#baseballGeme.countStrike(computerNumbers, userInput);
-    const ball = this.#baseballGeme.countBall(computerNumbers, userInput);
+    const strike = this.#baseballGame.countStrike(computerNumbers, userInput);
+    const ball = this.#baseballGame.countBall(computerNumbers, userInput);
 
     this.#outputView.printStrikeBall(strike, ball);
+    if (strike === THREE_STRIKE) this.askRestartGame();
+  }
+
+  askRestartGame() {
+    const { restart, quit } = COMMAND;
+    // 1, 2가 아닌 값 입력하면 예외처리
+    const userInput = (input) => {
+      if (Number(input) === restart) {
+        this.start();
+      }
+      if (Number(input) === quit) {
+        this.#outputView.quit();
+      }
+    };
+    this.#inputView.readRestartGame(userInput);
   }
 }
 
