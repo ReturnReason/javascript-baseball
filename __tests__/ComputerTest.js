622eb44faa14401f1a23502ce4c5c8219eb2e51a
ReturnReason <reasonz@daum.net>
2022-12-06 06:34:03 +0900

    test(Computer): makeGameNumbers 메서드 테스트

--- __tests__/ComputerTest.js
+++ __tests__/ComputerTest.js
@@ -1,10 +1,18 @@
 const Computer = require('../src/model/Computer');
 
 describe('✨ Computer 클래스 테스트', () => {
-  test('숫자 3개를 뽑을 수 있다.', () => {
+  test('makeGameNumbers 메서드 테스트 : 숫자 3개를 뽑을 수 있다.', () => {
     const computer = new Computer();
     const numbers = computer.makeGameNumbers();
 
     expect(numbers.length).toBe(3);
+  });
+
+  test('makeGameNumbers 메서드 테스트 : 뽑은 숫자 3개는 중복되지 않는 숫자로 이루어져있다.', () => {
+    const computer = new Computer();
+    const numbers = computer.makeGameNumbers();
+    const removeDuplicate = [...new Set(numbers)];
+
+    expect(removeDuplicate.length).toBe(3);
   });
 });
